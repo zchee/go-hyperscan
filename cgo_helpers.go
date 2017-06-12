b6d665a808cca5835e15fd7adf0bb5d28028299a
Koichi Shiraishi <zchee.io@gmail.com>
2017-06-12 15:22:34 +0900

    all: add copyright license header
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- cgo_helpers.go
+++ cgo_helpers.go
@@ -1,3 +1,7 @@
+// Copyright 2017 The go-hyperscan Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // WARNING: This file has automatically been generated on Mon, 12 Jun 2017 15:00:10 JST.
 // By https://git.io/c-for-go. DO NOT EDIT.
 
